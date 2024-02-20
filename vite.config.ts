6704d5831907dd4b19a426a64744dc5c74ed8808
yoshikouki <yoshikouki@gmail.com>
2024-02-20 23:53:54 +0900

    vite + hono

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
-})
+  plugins: [
+    devServer({
+      entry: "src/index.ts",
+    }),
+  ],
+});
