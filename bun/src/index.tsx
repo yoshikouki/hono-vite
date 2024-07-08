77a8fe8540a292142895c6649d51315797d0e715
yoshikouki <yoshikouki@gmail.com>
2024-07-08 20:45:19 +0900

    Setup @hono/react-renderer

---
+++ bun/src/index.tsx
@@ -0,0 +1,25 @@
+import { reactRenderer } from "@hono/react-renderer";
+import { Hono } from "hono";
+import type { ReactNode } from "react";
+
+const app = new Hono();
+
+app.get(
+	"*",
+	reactRenderer(({ children }: { children: ReactNode}) => {
+		return (
+			<html lang="en">
+				<body>
+					<h1>React + Hono</h1>
+					<div>{children}</div>
+				</body>
+			</html>
+		);
+	}),
+);
+
+app.get("/", (c) => {
+	return c.render(<p>Welcome!</p>);
+});
+
+export default app;
